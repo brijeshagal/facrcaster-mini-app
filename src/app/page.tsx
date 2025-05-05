a151c59e400599a083f189ddab1dbba67400c356
brijeshagal <brijeshagal@gmail.com>
2025-05-05 15:52:48 +0530

    update UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="text-black tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
