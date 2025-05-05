0c11549ae4dd7c1f0decf8aafe12913ef4f05f2f
brijeshagal <brijeshagal@gmail.com>
2025-05-05 16:01:30 +0530

    add frame loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { sdk } from '@farcaster/frame-sdk'
- 
-await sdk.actions.ready();
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
